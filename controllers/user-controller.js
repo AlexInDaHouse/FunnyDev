ca0fa668a9290daf8b4e3becb75332ca78afedd5
alexInDaHouse <lesha.aleshin1994@gmail.com>
2018-01-23 14:05:28 +0300

    user-controller: validate function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -8,36 +8,62 @@ class UserController {
         }
         
         if (req.body.password === req.body.password2) {
-            // let user = new User({
-            //     first_name: req.body.first_name || null,
-            //     last_name: req.body.last_name || null,
-            //     login: req.body.login,
-            //     password: req.body.password,
-            //     email: req.body.email,
-            //     avatar: 'default.png',
-            //     specialty: null,
-            //     role: 'user',
-            //     active: true
-            // });
+            let user = new User({
+                first_name: req.body.first_name || null,
+                last_name: req.body.last_name || null,
+                login: req.body.login,
+                password: req.body.password,
+                email: req.body.email,
+                avatar: 'default.png',
+                specialty: null,
+                role: 'user',
+                active: true
+            });
 
-            // let error = validate(user);
+            let errors = this.validate(user);
 
-            // if (error) {
+            if (errors) {
+            	let message = '';
 
-            // }
-            // else {
-
-            // }
-            res.end('Good');
+            	for (let error of errors) {
+            		message += error + '\n';
+            	}
+            	res.end(message);
+            }
+            else {
+            	res.end('OK');
+            }
+        }
+        else {
+        	res.end('Passwords don\'t match');
         }
 
-        console.log(req.body.password);
-        res.redirect(303, '/');
+        // res.redirect(303, '/');
     }
-}
 
-function validate(user) {
-    
+    // Returns array of errors
+    validate(user) {
+    	let errors = [];
+    	const errorMessages = {
+    		'login': 'Login incorrect!',
+    		'password': 'Password incorrect!',
+    		'email': 'Email incorrect!'
+    	};
+
+    	if (user.login.search(/^[A-Za-z0-9_-]{3,30}$/) !== 0) {
+    		errors.push(errorMessages['login']);
+    	}
+
+    	if (user.password.search(/^[A-Za-z0-9_-]{4,}$/) !== 0) {
+    		errors.push(errorMessages['password']);
+    	}
+
+    	if (user.email.search(/^[a-z0-9-]+@[a-z0-9]+.[a-z]{2,10}$/i) !== 0) {
+    		errors.push(errorMessages['email']);
+    	}
+
+    	return errors;
+    }
 }
 
 module.exports = new UserController();
